2f03d006878afd47ec50f60c3aba8f566dee39dc
viktoria284 <vikacharli@gmail.com>
2024-05-14 20:47:02 +0300

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import CardList from './Components/CardList'
 import './styles.css';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { UserProvider } from './Context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from "./Routes/Routes";
+
 
 function App() {
-
+  
   return (
     <>
-      <div className="main-title">
-        <h1>Main Page</h1>
-      </div>
-        <CardList />
+      <BrowserRouter>
+        <UserProvider>
+          <ToastContainer />
+          <AppRoutes />
+        </UserProvider>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
